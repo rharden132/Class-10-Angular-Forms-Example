1a3a9a9a0485f16537f79b220904cf6eadf2f0d7
William Wilder <76077183+WilderDev@users.noreply.github.com>
2021-10-05 10:52:59 -0500

    Create Book-Form-Template Starter Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookFormTemplateComponent } from './book-form-template/book-form-template.component';
+import { BookFormReactiveComponent } from './book-form-reactive/book-form-reactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookFormTemplateComponent,
+    BookFormReactiveComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/book-form-template/book-form-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-form-template',
+  templateUrl: './book-form-template.component.html',
+  styleUrls: ['./book-form-template.component.css']
+})
+export class BookFormTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
